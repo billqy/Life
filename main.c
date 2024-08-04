db17db2aaf56da1246bf1fe2afe9f3487ffe5f6d
Kram-Spr <willfr.spruill@gmail.com>
2024-08-03 23:59:41 -0400

    Refactor & intimate forces upgrade

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 
 typedef struct _Quark {
@@ -14,14 +15,17 @@ typedef struct _Quark {
 
 const int sWidth = 1600;
 const int sHeight = 1000;
-const int quarkCount = 1500;
+const int quarkCount = 1000;
 const float Gravity = 100;
+
+Vector2 screen = {sWidth, sHeight};
+Vector2 zeros = {0,0};
 
 
 void GenerateQuarks(Quark quarks[]) {
     for (int i = 0; i < quarkCount; i++) {
+        
         int typeNum = GetRandomValue(1, 4);
-        printf("%d", typeNum);
         char tempType;
         
         switch(typeNum) {
@@ -37,6 +41,9 @@ void GenerateQuarks(Quark quarks[]) {
             case 4:
                 tempType = 'R';
                 break;
+            default:
+                tempType = 'Y';
+                break;
         }
         
         Quark newQuark = { tempType, {GetRandomValue(100, 1500), GetRandomValue(100, 900)}, {0,0} };
@@ -44,106 +51,109 @@ void GenerateQuarks(Quark quarks[]) {
     }   
 }
 
+float DetermineAttraction(char selfType, char otherType) {
+    float modifier = 1;
+    
+    float interactionMatrix[4][4] = {
+            {1, 0.1, 0, -0.1}, // YxY, YxG, YxB, YxR
+            {-0.1, 1, 0.1, 0}, // GxY, GxG, GxB, GxR
+            {0, -0.1, 1, 0.1}, // BxY, BxG, BxB, BxR
+            {0.1, 0, -0.1, 1}  // RxY, RxG, RxB, RxR
+    };
+    
+    switch (selfType) {
+        case 'Y':
+            switch (otherType) {
+                case 'Y':
+                    modifier = interactionMatrix[0][0];
+                    break;
+                case 'G':
+                    modifier = interactionMatrix[0][1];
+                    break;
+                case 'B':
+                    modifier = interactionMatrix[0][2];
+                    break;
+                case 'R':
+                    modifier = interactionMatrix[0][3];
+                    break;
+                }
+        case 'G':
+            switch (otherType) {
+                case 'Y':
+                    modifier = interactionMatrix[1][0];
+                    break;
+                case 'G':
+                    modifier = interactionMatrix[1][1];
+                    break;
+                case 'B':
+                    modifier = interactionMatrix[1][2];
+                    break;
+                case 'R':
+                    modifier = interactionMatrix[1][3];
+                    break;
+                }
+        case 'B':
+            switch (otherType) {
+                case 'Y':
+                    modifier = interactionMatrix[2][0];
+                    break;
+                case 'G':
+                    modifier = interactionMatrix[2][1];
+                    break;
+                case 'B':
+                    modifier = interactionMatrix[2][2];
+                    break;
+                case 'R':
+                    modifier = interactionMatrix[2][3];
+                    break;
+                }
+        case 'R':
+            switch (otherType) {
+                case 'Y':
+                    modifier = interactionMatrix[3][0];
+                    break;
+                case 'G':
+                    modifier = interactionMatrix[3][1];
+                    break;
+                case 'B':
+                    modifier = interactionMatrix[3][2];
+                    break;
+                case 'R':
+                    modifier = interactionMatrix[3][3];
+                    break;
+                }
+            
+    }
+    
+    return modifier;
+}
+
 void UpdateQuarks(Quark* quark, float frametime, Quark quarks[]) {
     quark->pos = Vector2Add(quark->pos, Vector2Scale(quark->velo, frametime));
-    float F, fx, fy;
+    float F = 0, fx = 0, fy = 0;
     
     for (int i = 0; i < quarkCount; i++) {
         float G = Gravity;
-
-/* Interactions:
-        YxY = 0     YxG = 0.4   YxB = -0.1      YxR = -0.5
-        GxY = -0.2  GxG = 0.3   GxB = 0.1       GxR = -0.8
-        BxY = 0.3   BxG = 0.6   BxB = -0.2      BxR = 1
-        RxY = -1    RxG = -0.4  RxB = -.6       RxR = -0.1  */
         
-        switch(quark->type) {
-            case 'Y':
-                switch(quarks[i].type) {
-                    case 'Y':
-                        G *= 0;
-                        break;
-                    case 'G':
-                        G *= -0.6;
-                        break;
-                    case 'B':
-                        G *= 0;
-                        break;
-                    case 'R':
-                        G *= 0.8;
-                        break;
-                }
-                break;
-            case 'G':
-                switch(quarks[i].type) {
-                    case 'Y':
-                        G *= 0;
-                        break;
-                    case 'G':
-                        G *= -0.4;
-                        break;
-                    case 'B':
-                        G *= 0;
-                        break;
-                    case 'R':
-                        G *= 1;
-                        break;
-                }
-                break;
-            case 'B':
-                switch(quarks[i].type) {
-                    case 'Y':
-                        G *= 0;
-                        break;
-                    case 'G':
-                        G *= 0;
-                        break;
-                    case 'B':
-                        G *= 0;
-                        break;
-                    case 'R':
-                        G *= -0.3;
-                        break;
-                }
-                break;
-            case 'R':
-                switch(quarks[i].type) {
-                    case 'Y':
-                        G *= -0.8;
-                        break;
-                    case 'G':
-                        G *= -0.7;
-                        break;
-                    case 'B':
-                        G *= 0.3;
-                        break;
-                    case 'R':
-                        G *= 0.6;
-                        break;
-                }
-                break;
-        }
+        char selfType = quark->type;
+        char otherType = quarks[i].type;
         
-/*        if (quark->type == 'Y') {
-            if (quarks[i].type == 'Y') {
-                G *= 0;
-            } else {
-                G *= -1;
-            }
-        } else if (quark->type == 'R') {
-            if (quarks[i].type == 'R') {
-                G *= 1;
-            } else {
-                G *= 0;
-            }
-        }*/
-        
+        float modifier = DetermineAttraction(selfType, otherType);
         
         float dx = quarks[i].pos.x - quark->pos.x;
         float dy = quarks[i].pos.y - quark->pos.y;
         float d = sqrt((dx*dx) + (dy*dy));
-         
+        
+        G *= modifier;
         if (d > 0.01 && d < 300) {
+            if (d < 15) { 
+                if (G < 0) {
+                    G = 10000;
+                } else {
+                    G = -10000;
+                }
+            }
+            
             F = G * 1/d;
             fx = F * dx;
             fy = F * dy;
@@ -154,8 +164,21 @@ void UpdateQuarks(Quark* quark, float frametime, Quark quarks[]) {
         quark->velo = Vector2Scale(quark->velo, 0.99);
     }
         
-    if (quark->pos.x < 0 || quark->pos.x > sWidth) { quark->velo.x *= -1; }
-    if (quark->pos.y < 0 || quark->pos.y > sHeight) { quark->velo.y *= -1; }  
+    if (quark->pos.x < 0 || quark->pos.x > sWidth) {
+        quark->pos = Vector2Clamp(quark->pos, zeros, screen);
+        quark->velo.x *= -1; 
+    } else if (quark->pos.x > sWidth) {
+        quark->pos = Vector2Clamp(quark->pos, zeros, screen);
+        quark->velo.x *= -1; 
+    }
+    
+    if (quark->pos.y < 0 || quark->pos.y > sHeight) { 
+        quark->pos = Vector2Clamp(quark->pos, zeros, screen);
+        quark->velo.y *= -1; 
+    } else if (quark->pos.y > sHeight) {
+        quark->pos = Vector2Clamp(quark->pos, zeros, screen);
+        quark->velo.y *= -1;
+    }
 }
 
 
@@ -170,22 +193,29 @@ int main() {
     while (!WindowShouldClose()) {
         BeginDrawing();
         
+        char tQuarkLabel[] = "Quarks: ";
+        char tQuarkCount[10];
+        itoa(quarkCount, tQuarkCount, 10);
+        strcat(tQuarkLabel, tQuarkCount);
+        
         float frametime = GetFrameTime();
         
         for (int i = 0; i < quarkCount; i++) {
             UpdateQuarks(&quarks[i], frametime, quarks);
             
             if (quarks[i].type == 'Y') {
-                DrawRectangle(quarks[i].pos.x, quarks[i].pos.y, 7, 7, YELLOW);
+                DrawRectangle(quarks[i].pos.x, quarks[i].pos.y, 8, 8, YELLOW);
             } else if (quarks[i].type == 'G') {
-                DrawRectangle(quarks[i].pos.x, quarks[i].pos.y, 7, 7, GREEN);
+                DrawRectangle(quarks[i].pos.x, quarks[i].pos.y, 8, 8, GREEN);
             } else if (quarks[i].type == 'B') {
-                DrawRectangle(quarks[i].pos.x, quarks[i].pos.y, 7, 7, BLUE);
+                DrawRectangle(quarks[i].pos.x, quarks[i].pos.y, 8, 8, BLUE);
             } else if (quarks[i].type == 'R') {
-                DrawRectangle(quarks[i].pos.x, quarks[i].pos.y, 7, 7, RED);
+                DrawRectangle(quarks[i].pos.x, quarks[i].pos.y, 8, 8, RED);
             }
         }
         
+        DrawFPS(10,10);
+        DrawText(tQuarkLabel, 10, 35, 20, WHITE);
         ClearBackground(BLACK);
         
         EndDrawing();    
